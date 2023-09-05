a5620b9855075e5b9a8f09cd368993365f1fb3ce
opekope2 <opekope2@gmail.com>
2023-09-05 16:16:21 +0200

    Simplify leave.sh

--- .config/hypr/scripts/leave.sh
+++ .config/hypr/scripts/leave.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eo pipefail
-cd "$(dirname "$(readlink -f "$0")")"
+cd $(dirname $0)
 option="$(jq -r keys[] leave.json | rofi -dmenu -p "Leave session")"
 eval "$(jq -r ".\"$option\"" leave.json)"
